ccb56c192f3f49c1df8e7ac8baca20531513f419
GORAN GRBOVIĆ <goran.grbovic@kotor.me>
2025-05-28 17:35:40 +0200

    Dodato polje type u reservations, migracije i izmjene u bazi

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+        'time_slot_id',
+        'reservation_date',
+        'user_name',
+        'country',
+        'license_plate',
+        'vehicle_type_id',
+        'email',
+        'status',
+        'type', // drop_off ili pick_up
+    ];
 }
